525936011a93336357fd49510af1758b1316c728
Dalstam <daniel@web-reality.nl>
2020-05-05 11:17:13 +0200

    loader changed

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/*.ico");
+  eleventyConfig.addPassthroughCopy("./src/*.png");
+  eleventyConfig.addPassthroughCopy("./src/*.xml");
+  eleventyConfig.addPassthroughCopy("./src/*.json");
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/js");
+
 
   eleventyConfig.addLayoutAlias("main", "templates/mainDoc.njk");
   eleventyConfig.addLayoutAlias("inkoop", "templates/subDoc.njk");
